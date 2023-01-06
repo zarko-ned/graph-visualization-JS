70cc4cf97525368c2284e69cf9edad146569d228
Zarko <zarkoned@outloo.com>
2023-01-06 21:58:23 +0100

    added visualization file

---
+++ Visualization.js
@@ -0,0 +1,106 @@
+function forceSimulation(graph) {
+    return d3
+        .forceSimulation(graph.nodes)
+        .force(
+            "link",
+            d3
+                .forceLink()
+                .id(function (d) {
+                    return d.name;
+                })
+                .links(graph.links)
+        )
+
+        .force("charge", d3.forceManyBody().strength(-30))
+        .force("center", d3.forceCenter(width / 2, height / 2))
+        .on("tick", ticked);
+
+    function ticked() {
+        link
+            .attr("x1", function (d) {
+                return d.source.x;
+            })
+            .attr("y1", function (d) {
+                return d.source.y;
+            })
+            .attr("x2", function (d) {
+                return d.target.x;
+            })
+            .attr("y2", function (d) {
+                return d.target.y;
+            });
+
+        node
+            .attr("cx", function (d) {
+                return d.x;
+            })
+            .attr("cy", function (d) {
+                return d.y;
+            });
+        node.select("text")
+            .attr("x", function (d) {
+                return d.x;
+            })
+            .attr("y", function (d) {
+                return d.y;
+            });
+    }
+
+    
+
+}
+
+function simulationLink(graph, cssColor = 'red', width = 3) {
+    return svg
+        .append("g")
+        .attr("class", "links")
+        .selectAll("line")
+        .data(graph.links)
+        .enter()
+        .append("line")
+        .style("stroke", cssColor)
+        .attr("stroke-width", function (d) {
+            return width;
+        });
+}
+
+function simulationNode(graph) {
+    return svg
+        .append("g")
+        .attr("class", "nodes")
+        .selectAll("circle")
+        .data(graph.nodes)
+        .enter()
+        .append("circle")
+        .attr("r", 5)
+        .attr("fill", function (d) {
+            return "red";
+        })
+        .call(
+            d3
+                .drag()
+                .on("start", dragstarted)
+                .on("drag", dragged)
+                .on("end", dragended)
+        );
+}
+
+
+function showNodeName(graph) {
+    var node = svg.selectAll(".node")
+      .data(graph.nodes)
+      .enter().append("g")
+      .attr("class", "node")
+      .call(d3.drag()
+        .on("start", dragstarted)
+        .on("drag", dragged)
+        .on("end", dragended));
+  
+    node.append("circle")
+      .attr("r", 5);
+  
+    node.append("text")
+      .attr("dy", -3)
+      .text(function (d) { return d.name; });
+    return node;
+}
